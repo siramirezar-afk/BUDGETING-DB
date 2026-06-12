804db46d176bef3767a8cdf98d99ca66beca258e
siramirezar-afk <siramirezar@unal.edu.co>
2026-06-12 17:45:35 -0500

    Add initial data for users and categories

---
+++ queries.sql
@@ -0,0 +1,6 @@
+-- Primer dato (Luna referencia 😮)
+INSERT INTO users (userID, userName, email, password) 
+VALUES (1, 'Luna', 'luna@email.com', 'difficultpassword123');
+
+INSERT INTO categories (categoryID, cateName, cateType, description, tagColour) 
+VALUES (1, 'Food', 'Expense', 'Daily meals and snacks', '#FF5733');
